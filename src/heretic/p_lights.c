827b86d80fec61439259a30a5729ac804981db7b
Noseey <omega_1986@hotmail.com>
2024-12-01 16:51:41 +0100

    Revert "Heretic: Raw disabling of Sector Light Flash"
    This reverts commit e735ab2d681dedb1729e582ea3861bed8e29ce05.

--- src/heretic/p_lights.c
+++ src/heretic/p_lights.c
@@ -38,21 +38,19 @@ void T_LightFlash(thinker_t *thinker)
 {
     lightflash_t *flash = (lightflash_t *) thinker;
 
-    flash->sector->lightlevel = flash->maxlight;
+    if (--flash->count)
+        return;
 
-    // if (--flash->count)
-    //     return;
-
-    // if (flash->sector->lightlevel == flash->maxlight)
-    // {
-    //     flash->sector->lightlevel = flash->minlight;
-    //     flash->count = (P_Random() & flash->mintime) + 1;
-    // }
-    // else
-    // {
-    //     flash->sector->lightlevel = flash->maxlight;
-    //     flash->count = (P_Random() & flash->maxtime) + 1;
-    // }
+    if (flash->sector->lightlevel == flash->maxlight)
+    {
+        flash->sector->lightlevel = flash->minlight;
+        flash->count = (P_Random() & flash->mintime) + 1;
+    }
+    else
+    {
+        flash->sector->lightlevel = flash->maxlight;
+        flash->count = (P_Random() & flash->maxtime) + 1;
+    }
 
 }
 
@@ -99,21 +97,19 @@ void T_StrobeFlash(thinker_t *thinker)
 {
     strobe_t *flash = (strobe_t *) thinker;
 
-    flash->sector->lightlevel = flash->maxlight;
+    if (--flash->count)
+        return;
 
-    // if (--flash->count)
-    //     return;
-
-    // if (flash->sector->lightlevel == flash->minlight)
-    // {
-    //     flash->sector->lightlevel = flash->maxlight;
-    //     flash->count = flash->brighttime;
-    // }
-    // else
-    // {
-    //     flash->sector->lightlevel = flash->minlight;
-    //     flash->count = flash->darktime;
-    // }
+    if (flash->sector->lightlevel == flash->minlight)
+    {
+        flash->sector->lightlevel = flash->maxlight;
+        flash->count = flash->brighttime;
+    }
+    else
+    {
+        flash->sector->lightlevel = flash->minlight;
+        flash->count = flash->darktime;
+    }
 
 }
 
@@ -247,27 +243,25 @@ void T_Glow(thinker_t *thinker)
 {
     glow_t *g = (glow_t *) thinker;
 
-    g->sector->lightlevel = g->maxlight;
-
-    // switch (g->direction)
-    // {
-    //     case -1:               // DOWN
-    //         g->sector->lightlevel -= GLOWSPEED;
-    //         if (g->sector->lightlevel <= g->minlight)
-    //         {
-    //             g->sector->lightlevel += GLOWSPEED;
-    //             g->direction = 1;
-    //         }
-    //         break;
-    //     case 1:                // UP
-    //         g->sector->lightlevel += GLOWSPEED;
-    //         if (g->sector->lightlevel >= g->maxlight)
-    //         {
-    //             g->sector->lightlevel -= GLOWSPEED;
-    //             g->direction = -1;
-    //         }
-    //         break;
-    // }
+    switch (g->direction)
+    {
+        case -1:               // DOWN
+            g->sector->lightlevel -= GLOWSPEED;
+            if (g->sector->lightlevel <= g->minlight)
+            {
+                g->sector->lightlevel += GLOWSPEED;
+                g->direction = 1;
+            }
+            break;
+        case 1:                // UP
+            g->sector->lightlevel += GLOWSPEED;
+            if (g->sector->lightlevel >= g->maxlight)
+            {
+                g->sector->lightlevel -= GLOWSPEED;
+                g->direction = -1;
+            }
+            break;
+    }
 }
 
 void P_SpawnGlowingLight(sector_t * sector)
